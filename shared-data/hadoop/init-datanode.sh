ac9ca7914b0feabfa93bdffe92f14eea32406e97
Hilton Martins <hiltonmbr@gmail.com>
2025-04-16 21:22:09 -0300

    fix permissions

--- shared-data/hadoop/init-datanode.sh
+++ shared-data/hadoop/init-datanode.sh
@@ -3,3 +3,5 @@ rm -rf /opt/hadoop/data/dataNode/*
 chown -R hadoop:hadoop /opt/hadoop/data/dataNode
 chmod 755 /opt/hadoop/data/dataNode
 hdfs datanode
+hdfs hadoop fs -mkdir /data
+hdfs hadoop fs -chown abc /data
--- shared-data/hadoop/start-hdfs.sh
+++ shared-data/hadoop/start-hdfs.sh
@@ -2,5 +2,6 @@
 if [ ! -d "/opt/hadoop/data/nameNode/current" ]; then
     echo "Formatting NameNode..."
     hdfs namenode -format
+    hdfs dfs -chmod 777 /
 fi
 hdfs namenode
